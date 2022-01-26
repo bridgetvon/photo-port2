9ab3851b108dccef2f0fca680b46a13595247cfa
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-26 10:48:42 -0700

    modal functionality added with open and close

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,7 +1,15 @@
 import React, { useState } from 'react';
-// import Modal from '../Modal';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
+  //use the use state hook to manage the current photo state and make this data accessible to modal 
+  const [currentPhoto, setCurrentPhoto ] = useState();
+    //create hook to conditionally render the modal based on if an image is clicked 
+  //set the initial state of the modal is open to false because we dont want the modal to open until the user has clicked on an image 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+  
   const [photos] = useState([
     {
       name: 'Grocery aisle',
@@ -118,18 +126,37 @@ const PhotoList = ({ category }) => {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie'
     }
   ]);
+
 //go through each photo in the photos array trying to find every photo that matches a category selected by the user
 //if it matches the condition its returned in an array and assigned to current photos
   const currentPhotos = photos.filter(photo => photo.category === category);
 
+
+
+  const toggleModal = (image, i) => {
+    //use the spread operator to add index: i key value pair 
+    //now current photo contains two data points for the modal we can pass current 
+    //phot as a prop to the modal 
+    setCurrentPhoto({ ...image, index: i});
+    //modal to open when a user clicks an image 
+    //toogle ismodal open from false to true with ! operator
+    setIsModalOpen(!isModalOpen);
+  }
+
+  
+
   return (
     <div>
+      {isModalOpen && (
+       <Modal currentPhoto={currentPhoto} onClose={toggleModal} 
+       />)}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
-            src={require(`../../assets/small/${category}/${i}.jpg`).default}
+            src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
